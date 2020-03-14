099645a579a5378c06cf3200cf63001e6affdc62
Francisco Thenoux <tenumaster@gmail.com>
2020-03-13 21:37:30 -0300

    Updates

---
+++ precache-manifest.b4ad52a6269874f5f891d9a6d0ad5906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31df9c78e7980f825c3abfbdea6d3165",
+    "url": "/react-to-the-movies/index.html"
+  },
+  {
+    "revision": "c757a2a87a87dbc9016d",
+    "url": "/react-to-the-movies/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f5deca55dec4abb81a00",
+    "url": "/react-to-the-movies/static/js/2.e0b67c21.chunk.js"
+  },
+  {
+    "revision": "87a373afb890b36096d72d80de5eeb84",
+    "url": "/react-to-the-movies/static/js/2.e0b67c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c757a2a87a87dbc9016d",
+    "url": "/react-to-the-movies/static/js/main.1afaf058.chunk.js"
+  },
+  {
+    "revision": "871fbe8304c49b4ae9cb",
+    "url": "/react-to-the-movies/static/js/runtime-main.4382375a.js"
+  }
+]);
